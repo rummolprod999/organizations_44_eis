23934d45f34288501f2b9cf8153073753c820868
rummolprod999 <rummolprod999@gmail.com>
2019-10-22 09:26:21 +0300

    Initial commit

---
+++ parser_org.py
@@ -0,0 +1,261 @@
+import pymysql
+import logging
+import datetime
+import os
+from warnings import filterwarnings
+from connect_to_db import connect_bd
+
+EXECUTE_PATH = os.path.dirname(os.path.abspath(__file__))
+LOG_D = 'log_organization'
+LOG_DIR = f"{EXECUTE_PATH}/{LOG_D}"
+TEMP_D = 'temp_organization'
+TEMP_DIR = f"{EXECUTE_PATH}/{TEMP_D}"
+if not os.path.exists(LOG_DIR):
+    os.mkdir(LOG_DIR)
+# file_log = './log_organization/organization_ftp_' + str(datetime.date.today()) + '.log'
+file_log = f'{LOG_DIR}/organization_ftp_{str(datetime.date.today())}.log'
+filterwarnings('ignore', category=pymysql.Warning)
+# file_log = './log_organization/organization_ftp_' + str(datetime.date.today()) + '.log'
+logging.basicConfig(level=logging.DEBUG, filename=file_log,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+SUFFIX = '_from_ftp'
+SUFFIXOD = ''
+DB = 'tender'
+
+if __name__ == "__main__":
+    print('Привет, этот файл только для импортирования!')
+
+
+def generator_univ(c):
+    if type(c) == list:
+        for i in c:
+            yield i
+    else:
+        yield c
+
+
+def get_el(d, *kwargs):
+    res = ''
+    l = len(kwargs)
+    if l == 1:
+        try:
+            res = d[kwargs[0]]
+        except Exception:
+            res = ''
+    elif l == 2:
+        try:
+            res = d[kwargs[0]][kwargs[1]]
+        except Exception:
+            res = ''
+    elif l == 3:
+        try:
+            res = d[kwargs[0]][kwargs[1]][kwargs[2]]
+        except Exception:
+            res = ''
+    elif l == 4:
+        try:
+            res = d[kwargs[0]][kwargs[1]][kwargs[2]][kwargs[3]]
+        except Exception:
+            res = ''
+    elif l == 5:
+        try:
+            res = d[kwargs[0]][kwargs[1]][kwargs[2]][kwargs[3]][kwargs[4]]
+        except Exception:
+            res = ''
+    elif l == 6:
+        try:
+            res = d[kwargs[0]][kwargs[1]][kwargs[2]][kwargs[3]][kwargs[4]][kwargs[5]]
+        except Exception:
+            res = ''
+    if res is None:
+        res = ''
+    return res
+
+
+def logging_parser(*kwargs):
+    s_log = str(datetime.datetime.now()) + ' '
+    for i in kwargs:
+        s_log += f'{str(i)} '
+    s_log += '\n\n'
+    with open(file_log, 'a') as flog:
+        flog.write(s_log)
+
+
+class Organization:
+    log_insert = 0
+    log_update = 0
+    regnumber_null = 0
+    add_new_customer = 0
+    update_new_customer = 0
+
+    def __init__(self, org):
+        self.org = org
+
+    def get_org(self):
+        if 'export' in self.org:
+            if 'nsiOrganizationList' in self.org['export']:
+                if 'nsiOrganization' in self.org['export']['nsiOrganizationList']:
+                    orgs = generator_univ(self.org['export']['nsiOrganizationList']['nsiOrganization'])
+                else:
+                    return []
+            else:
+                return []
+        else:
+            return []
+        return orgs
+
+    @staticmethod
+    def regNumber(og):
+        return get_el(og, 'regNumber')
+
+    @staticmethod
+    def inn(og):
+        return get_el(og, 'INN')
+
+    @staticmethod
+    def kpp(og):
+        return get_el(og, 'KPP')
+
+    @staticmethod
+    def ogrn(og):
+        return get_el(og, 'OGRN')
+
+    @staticmethod
+    def region_code(og):
+        r_c = get_el(og, 'factualAddress', 'region', 'kladrCode')
+        if not r_c:
+            r_c = get_el(og, 'factualAddress', 'city', 'kladrCode')
+        if not r_c:
+            r_c = get_el(og, 'factualAddress', 'area', 'kladrCode')
+        if r_c:
+            r_c = r_c[:2]
+        return r_c
+
+    @staticmethod
+    def full_name(og):
+        return get_el(og, 'fullName')
+
+    @staticmethod
+    def short_name(og):
+        return get_el(og, 'shortName')
+
+    @staticmethod
+    def postal_address(og):
+        return get_el(og, 'postalAddress')
+
+    @staticmethod
+    def phone(og):
+        return get_el(og, 'phone')
+
+    @staticmethod
+    def fax(og):
+        return get_el(og, 'fax')
+
+    @staticmethod
+    def email(og):
+        return get_el(og, 'email')
+
+    @staticmethod
+    def contact_name(og):
+        lastName = get_el(og, 'contactPerson', 'lastName')
+        firstName = get_el(og, 'contactPerson', 'firstName')
+        middleName = get_el(og, 'contactPerson', 'middleName')
+        return '{0} {1} {2}'.format(firstName, middleName, lastName)
+
+
+def parser_o(org, path):
+    regNumber = Organization.regNumber(org)
+    inn = Organization.inn(org)
+    if not regNumber:
+        Organization.regnumber_null += 1
+        logging_parser('У организации нет regnum', path, inn)
+        return
+    kpp = Organization.kpp(org)
+    ogrn = Organization.ogrn(org)
+    region_code = Organization.region_code(org)
+    full_name = Organization.full_name(org)
+    short_name = Organization.short_name(org)
+    postal_address = Organization.postal_address(org)
+    phone = Organization.phone(org)
+    fax = Organization.fax(org)
+    email = Organization.email(org)
+    contact_name = Organization.contact_name(org)
+    contracts_count = 0
+    contracts223_count = 0
+    contracts_sum = 0.0
+    contracts223_sum = 0.0
+    con = connect_bd(DB)
+    cur = con.cursor()
+    cur.execute(f"""SELECT * FROM od_customer{SUFFIX} WHERE regNumber = %s """, (regNumber,))
+    resultregnum = cur.fetchone()
+    if not resultregnum:
+        query1 = f"""INSERT INTO od_customer{SUFFIX} SET regNumber = %s, inn = %s, kpp = %s, contracts_count = %s, 
+                contracts223_count = %s, contracts_sum = %s, contracts223_sum = %s, ogrn = %s, region_code = %s, 
+                full_name = %s, short_name = %s, postal_address = %s, phone = %s, fax = %s, email = %s, 
+                contact_name = %s"""
+        value1 = (
+            regNumber, inn, kpp, contracts_count, contracts223_count, contracts_sum, contracts223_sum, ogrn,
+            region_code,
+            full_name, short_name, postal_address, phone, fax, email, contact_name)
+        cur.execute(query1, value1)
+        Organization.log_insert += 1
+        cur.execute(f"""SELECT id FROM od_customer{SUFFIXOD} WHERE regNumber=%s""", (regNumber,))
+        res_c = cur.fetchone()
+        if not res_c:
+            query3 = f"""INSERT INTO od_customer{SUFFIXOD} SET regNumber = %s, inn = %s, kpp = %s, contracts_count = %s, 
+                            contracts223_count = %s, contracts_sum = %s, contracts223_sum = %s, ogrn = %s, 
+                            region_code = %s, 
+                            full_name = %s, postal_address = %s, phone = %s, fax = %s, email = %s, 
+                            contact_name = %s, short_name = %s"""
+            value3 = (
+                regNumber, inn, kpp, contracts_count, contracts223_count, contracts_sum, contracts223_sum, ogrn,
+                region_code,
+                full_name, postal_address, phone, fax, email, contact_name, short_name)
+            cur.execute(query3, value3)
+            Organization.add_new_customer += 1
+    else:
+        query2 = f"""UPDATE od_customer{SUFFIX} SET inn = %s, kpp = %s, contracts_count = %s, contracts223_count = %s, 
+                contracts_sum = %s, contracts223_sum = %s, ogrn = %s, region_code = %s, full_name = %s, short_name = %s,
+                postal_address = %s, phone = %s, fax = %s, email = %s, 
+                contact_name = %s WHERE regNumber = %s"""
+        value2 = (
+            inn, kpp, contracts_count, contracts223_count, contracts_sum, contracts223_sum, ogrn, region_code,
+            full_name, short_name,
+            postal_address, phone, fax, email, contact_name, regNumber)
+        cur.execute(query2, value2)
+        Organization.log_update += 1
+        cur.execute(f"""SELECT id FROM od_customer{SUFFIXOD} WHERE regNumber=%s""", (regNumber,))
+        res_cc = cur.fetchone()
+        if not res_cc:
+            query4 = f"""INSERT INTO od_customer{SUFFIXOD} SET regNumber = %s, inn = %s, kpp = %s, contracts_count = %s, 
+                                    contracts223_count = %s, contracts_sum = %s, contracts223_sum = %s, ogrn = %s, 
+                                    region_code = %s, 
+                                    full_name = %s, postal_address = %s, phone = %s, fax = %s, email = %s, 
+                                    contact_name = %s, short_name = %s"""
+            value4 = (
+                regNumber, inn, kpp, contracts_count, contracts223_count, contracts_sum, contracts223_sum, ogrn,
+                region_code,
+                full_name, postal_address, phone, fax, email, contact_name, short_name)
+            cur.execute(query4, value4)
+            Organization.add_new_customer += 1
+        # query4 = """UPDATE od_customer{0} SET inn = %s, kpp = %s, contracts_count = %s, contracts223_count = %s,
+        #                 contracts_sum = %s, contracts223_sum = %s, ogrn = %s, region_code = %s, full_name = %s,
+        #                 postal_address = %s, phone = %s, fax = %s, email = %s,
+        #                 contact_name = %s WHERE regNumber = %s""".format(SUFFIX)
+        # cur.execute(query4, value2)
+        # Organization.update_new_customer += 1
+    cur.close()
+    con.close()
+
+
+def parser(doc, path_xml):
+    # global file_log
+    o = Organization(doc)
+    orgs = o.get_org()
+    if not orgs:
+        logging_parser('В файле нет списка организаций:', path_xml)
+    for org in orgs:
+        try:
+            parser_o(org, path_xml)
+        except Exception:
+            logging.exception(f'Ошибка парсера {path_xml}')
---
+++ parser_organization.py
@@ -0,0 +1,216 @@
+import ftplib
+import pymysql
+import logging
+import datetime
+import zipfile
+import os
+import shutil
+import xmltodict
+import parser_org
+import timeout_decorator
+from connect_to_db import connect_bd
+
+# EXECUTE_PATH = os.path.dirname(os.path.abspath(__file__))
+# LOG_D = 'log_organization'
+# LOG_DIR = f"{EXECUTE_PATH}/{LOG_D}"
+# TEMP_D = 'temp_prot'
+# TEMP_DIR = f"{EXECUTE_PATH}/{TEMP_D}"
+# if not os.path.exists(LOG_DIR):
+#     os.mkdir(LOG_DIR)
+# file_log = './log_organization/organization_ftp_' + str(datetime.date.today()) + '.log'
+file_log = parser_org.file_log
+temp_dir = parser_org.TEMP_DIR
+logging.basicConfig(level=logging.DEBUG, filename=file_log,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+count_good = 0
+count_bad = 0
+except_file = ()
+unic_files = []
+
+
+def get_xml_to_dict(filexml, dirxml):
+    """
+    :param filexml: имя xml
+    :param dirxml: путь к локальному файлу
+    :return:
+    """
+    global count_good
+    global count_bad
+
+    path_xml = dirxml + '/' + filexml
+    # print(path_xml)
+    with open(path_xml) as fd:
+        try:
+            s = fd.read()
+            s = s.replace("ns2:", "")
+            s = s.replace("oos:", "")
+            doc = xmltodict.parse(s)
+            parser_org.parser(doc, path_xml)
+            count_good += 1
+            # with open(file_count_god, 'a') as good:
+            #     good.write(str(count_good) + '\n')
+
+        except Exception as ex:
+            logging.exception("Ошибка: ")
+            with open(file_log, 'a') as flog:
+                flog.write(f'Ошибка конвертации в словарь {str(ex)} {path_xml}\n\n\n')
+            count_bad += 1
+            # with open(file_count_bad, 'a') as bad:
+            #     bad.write(str(count_bad) + '\n')
+            # return
+
+
+def bolter(file, l_dir):
+    """
+    :param file: файл для проверки на черный список
+    :param l_dir: директория локального файла
+    :return: Если файл есть в черном списке - выходим
+    """
+    # print(f)
+    try:
+        get_xml_to_dict(file, l_dir)
+    except Exception as exppars:
+        logging.exception("Ошибка: ")
+        with open(file_log, 'a') as flog:
+            flog.write(f'Не удалось пропарсить файл {str(exppars)} {file}\n')
+
+
+def get_list_ftp(path_parse):
+    host = 'ftp.zakupki.gov.ru'
+    ftpuser = 'free'
+    password = 'free'
+    ftp2 = ftplib.FTP(host)
+    ftp2.set_debuglevel(0)
+    ftp2.encoding = 'utf8'
+    ftp2.login(ftpuser, password)
+    ftp2.cwd(path_parse)
+    data = ftp2.nlst()
+    array_ar = []
+    for i in data:
+        if i.find('2016') != -1 or i.find('2017') != -1 or i.find('2018') != -1 or i.find('2019') != -1:
+            array_ar.append(i)
+
+    return array_ar
+
+
+def extract_prot(m, path_parse1):
+    """
+    :param m: имя архива с контрактами
+    :param path_parse1: путь до папки с архивом
+    """
+    l = get_ar(m, path_parse1)
+    if l:
+        # print(l)
+        r_ind = l.rindex('.')
+        l_dir = l[:r_ind]
+        os.mkdir(l_dir)
+        try:
+            z = zipfile.ZipFile(l, 'r')
+            z.extractall(l_dir)
+            z.close()
+        except UnicodeDecodeError as ea:
+            print('Не удалось извлечь архив ' + str(ea) + ' ' + l)
+            with open(file_log, 'a') as floga:
+                floga.write(f'Не удалось извлечь архив {str(ea)} {l}\n')
+            try:
+                os.system('unzip %s -d %s' % (l, l_dir))
+            except Exception as ear:
+                with open(file_log, 'a') as flogb:
+                    flogb.write(f'Не удалось извлечь архив альтернативным методом {str(ear)} {l}\n')
+                return
+        except Exception as e:
+            logging.exception("Ошибка: ")
+            with open(file_log, 'a') as flogc:
+                flogc.write(f'Не удалось извлечь архив {str(e)} {l}\n')
+            return
+
+        try:
+            file_list = os.listdir(l_dir)
+        except Exception as ex:
+            logging.exception("Ошибка: ")
+            with open(file_log, 'a') as flog:
+                flog.write(f'Не удалось получить список файлов {str(ex)} {l_dir}\n')
+        else:
+            for f in file_list:
+                bolter(f, l_dir)
+
+        os.remove(l)
+        shutil.rmtree(l_dir, ignore_errors=True)
+
+
+@timeout_decorator.timeout(300)
+def down_timeout(m, path_parse1):
+    host = 'ftp.zakupki.gov.ru'
+    ftpuser = 'free'
+    password = 'free'
+    ftp2 = ftplib.FTP(host)
+    ftp2.set_debuglevel(0)
+    ftp2.encoding = 'utf8'
+    ftp2.login(ftpuser, password)
+    ftp2.cwd(path_parse1)
+    local_f = '{0}/{1}'.format(temp_dir, str(m))
+    lf = open(local_f, 'wb')
+    ftp2.retrbinary('RETR ' + str(m), lf.write)
+    lf.close()
+    return local_f
+
+
+def get_ar(m, path_parse1):
+    """
+    :param m: получаем имя архива
+    :param path_parse1: получаем путь до архива
+    :return: возвращаем локальный путь до архива или 0 в случае неудачи
+    """
+    retry = True
+    count = 0
+    while retry:
+        try:
+            lf = down_timeout(m, path_parse1)
+            retry = False
+            return lf
+        except Exception as ex:
+            logging.exception("Ошибка: ")
+            with open(file_log, 'a') as flog:
+                flog.write(f'Не удалось скачать архив {str(ex)} {m}\n')
+            if count > 10:
+                return 0
+            count += 1
+
+
+def main():
+    with open(file_log, 'a') as flog:
+        flog.write(f'Время начала работы парсера: {str(datetime.datetime.now())}\n')
+    shutil.rmtree(temp_dir, ignore_errors=True)
+    os.mkdir(temp_dir)
+    path_parse = 'fcs_nsi/nsiOrganization/'
+    try:
+        # получаем список архивов
+        arr_con = get_list_ftp(path_parse)
+        for j in arr_con:
+            try:
+                extract_prot(j, path_parse)
+            except Exception as exc:
+                logging.exception("Ошибка: ")
+                with open(file_log, 'a') as flog:
+                    flog.write(f'Ошибка в экстракторе и парсере {str(exc)} {j}\n')
+                continue
+
+    except Exception as ex:
+        logging.exception("Ошибка: ")
+        with open(file_log, 'a') as flog:
+            flog.write(f'Не удалось получить список архивов {str(ex)} {path_parse}\n')
+    with open(file_log, 'a') as flog:
+        flog.write(f'Добавлено заказчиков: {str(parser_org.Organization.log_insert)}\n')
+        flog.write(f'Обновлено заказчиков: {str(parser_org.Organization.log_update)}\n')
+        flog.write(f'Заказчиков без RegNumber: {str(parser_org.Organization.regnumber_null)}\n')
+        flog.write(f'Добавлено в od_customer: {str(parser_org.Organization.add_new_customer)}\n')
+        flog.write(f'Обновлено в od_customer: {str(parser_org.Organization.update_new_customer)}\n')
+        flog.write(f'Время окончания работы парсера: {str(datetime.datetime.now())}\n\n\n')
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as exm:
+        with open(file_log, 'a') as flogm:
+            flogm.write(f'Ошибка в функции main: {str(exm)}\n\n\n')
