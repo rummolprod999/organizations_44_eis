12d0ec07bcf64261d3150ebb53e94d4e81be0bcb
rummolprod999 <rummolprod999@gmail.com>
2024-07-14 13:46:54 +0300

    update

--- connect_to_db.py
+++ connect_to_db.py
@@ -5,12 +5,12 @@ if __name__ == "__main__":
 
 
 def connect_bd_localhost(baza):
-    con = pymysql.connect(host="localhost", user="test", passwd="1234", db=baza, charset='utf8',
+    con = pymysql.connect(host="localhost", user="test", passwd="11111111", db=baza, charset='utf8',
                           init_command='SET NAMES UTF8', cursorclass=pymysql.cursors.DictCursor, autocommit=True)
     return con
 
 
 def connect_bd(baza):
-    con = pymysql.connect(host="localhost", port=3306, user="root", passwd="1234", db=baza, charset='utf8',
+    con = pymysql.connect(host="localhost", port=3306, user="parser", passwd="111111", db=baza, charset='utf8',
                           init_command='SET NAMES UTF8', cursorclass=pymysql.cursors.DictCursor, autocommit=True)
     return con
